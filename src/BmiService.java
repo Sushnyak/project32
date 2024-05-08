8ab8059169ec2061b87d62142bdfedb2ccfd3f00
Даниил <danilazakharov2003@gmail.com>
2024-05-08 22:13:21 +0300

    initialcommit

---
+++ src/BmiService.java
@@ -0,0 +1,11 @@
+public class BmiService {
+
+    public int calculate(double heihgtMeters, int weight){
+
+    double bmiIndex = weight / Math.pow(heihgtMeters,2);
+    return (int) bmiIndex;
+
+
+    }
+
+}
